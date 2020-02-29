d611912793eadaff3521aeda623380b58f66bc47
prashant523580 <prashant.thapa62758@gmail.com>
2020-02-29 07:30:50 +0545

    Dynamic Calendar using HTML CSS JS

--- calendar/js/main.js
+++ calendar/js/main.js
@@ -1,21 +1,64 @@
 var date = new Date();
-// date.setDate(24);
-console.log(date.getMonth());
-var months = [
-    "january",
-    "feburary",
-    "march",
-    "may",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december"
-];
-console.log(months[date.getMonth()]);
-document.getElementById("date").innerHTML = date.toDateString() + " " + date.toTimeString();
-document.getElementById("month").innerHTML = months[date.getMonth()];
+
+function BodyLoad() {
+    var lastDateofMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+    var previousDateofMonth = new Date(
+        date.getFullYear(),
+        date.getMonth(), 0
+    ).getDate();
+    // date.setDate(1);
+    var day = date.getDay();
+    // console.log(lastDateofMonth);
+    // console.log(date.getMonth());
+    var today = new Date();
+    var months = [
+        "January",
+        "Feburary",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    // console.log(months[date.getMonth()]); 
+    document.getElementById("date").innerHTML = date.toDateString();
+    document.getElementById("month").innerHTML = months[date.getMonth()];
+
+    var cells = "<ul>";
+
+    for (var x = day; x > 0; x--) {
+        cells += "<li class='previous-date'>" + (previousDateofMonth - x + 1) + "</li>";
+    }
+    for (var i = 1; i <= lastDateofMonth; i++) {
+        if (i === today.getDate() && date.getMonth() === today.getMonth()) {
+
+            cells += "<li class='active'>" + i + "</li>";
+        } else {
+            cells += "<li>" + i + "</li>";
+        }
+
+    }
+
+    document.getElementsByClassName("days")[0].innerHTML = cells;
+}
+
+function changeMonth(change) {
+    console.log(change);
+    if (change === "prev") {
+        date.setMonth(date.getMonth() - 1);
+        // BodyLoad();
+    } else {
+        date.setMonth(date.getMonth() + 1);
+        // BodyLoad();
+    }
+    BodyLoad();
+}
