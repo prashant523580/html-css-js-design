0e5eb54c3ff3f685490a80fa40b14bb6627a4299
prashant523580 <prashant.thapa62758@gmail.com>
2020-03-16 15:45:35 +0545

    Nav bar responsive design HTML CSS & JAVASCRIPT

--- nav-bar/js/main.js
+++ nav-bar/js/main.js
@@ -1,6 +1,18 @@
+window.onscroll = function() { scroll() };
+
+function scroll() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.padding = "0px 30px";
+        // document.getElementById("nav-links").style.fontSize = "17px";
+    } else {
+        document.getElementById("navbar").style.padding = "20px 30px";
+        // document.getElementById("nav-links").style.fontSize = "17.5px";
+    }
+}
+
 function fun(y) {
     y.classList.toggle("change");
-    var x = document.getElementById("nav");
+    var x = document.getElementById("navbar");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
